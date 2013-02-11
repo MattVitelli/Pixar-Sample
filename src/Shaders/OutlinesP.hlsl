5277cf92d09a50e9731dd6b58d849e6a2d145b69
Matt Vitelli <matthew.vitelli@gmail.com>
2013-02-10 22:55:51 -0800

    Added a bunch of features and cleaned up code

--- src/Shaders/OutlinesP.hlsl
+++ src/Shaders/OutlinesP.hlsl
@@ -7,15 +7,12 @@ float4 main(float2 TC : TEXCOORD0, uniform sampler BaseMap : register(S0), unifo
     float variance = 0;
 
     avgValue = tex2D(BaseMap, TC);
-    //avgValue /= dot(avgValue,1);
     for(int i = -1; i <= 1; i++)
     {
 		for(int j = -1; j <= 1; j++)
 		{
 			float3 samp = tex2D(BaseMap, TC+float2(i,j)*InvRes);
-			//samp /= dot(samp, 1);
-			float v = dot(samp-avgValue, samp-avgValue);//length(samp - avgValue);
-			//v *= v;
+			float v = dot(samp-avgValue, samp-avgValue);
 			variance += v;
 			weight++;
 		}
@@ -23,5 +20,4 @@ float4 main(float2 TC : TEXCOORD0, uniform sampler BaseMap : register(S0), unifo
     variance /= (weight-1);
     
     return pow(exp((variance)/-0.24),sharpness);
-    //return (variance>Threshold) ? 0 : 1;
 }
